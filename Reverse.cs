b9e24b83ae69acef9c8815aba2e1afd542246dcc
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 11:44:06 +0530

    Adding program to reverse the number

---
+++ Reverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class Reverse
+    {
+        public static void ReverseNum()
+        {
+            int n, rem, reverse = 0;
+            Console.WriteLine("Enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Revrsed number is " + reverse);
+        }
+    }
+    
+}
