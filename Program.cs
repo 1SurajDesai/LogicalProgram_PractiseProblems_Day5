eab9ce85d98591a6b241a15a5ba76deb833dfc22
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 11:32:16 +0530

    Adding Program.cs file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LogicalProgramming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Fibobacci.DisplayFibonacci();
+            //Reverse.ReverseNum();
+            //PerfectNo.FindPerfectNo();
+            //PrimeNo.CheckPrimeNo();
+            //StopwatchProgram.CalElapsedTime();
+        }
+    }
+}
