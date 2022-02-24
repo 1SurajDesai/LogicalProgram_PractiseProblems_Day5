fcb405c4a09eb0775edaa6c03a39beb1bdfa97ba
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 11:38:47 +0530

    Adding program to get perfect numbers

---
+++ PerfectNo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class PerfectNo
+    {
+        public static void FindPerfectNo()
+        {
+            int num, sum, mn, mx;
+            Console.WriteLine("Finding perfect numbers in given series");
+            Console.WriteLine("Enter starting range:");
+            mn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter ending range:");
+            mx = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Perfect numbers are:");
+
+            for (num = mn; num <= mx; num++)
+            {
+                int i = 1;
+                sum = 0;
+                while (i <= num/2)
+                {
+                    if (num % i == 0)
+                        sum += i;
+                    i++;
+                }
+                if (sum == num)
+                    Console.Write(num + " ");
+            }
+        }
+    }
+}
