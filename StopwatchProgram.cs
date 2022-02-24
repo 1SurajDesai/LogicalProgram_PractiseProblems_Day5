01f871aa3f10c69016fb7518b002d68222fa47bd
1SurajDesai <surajdesai219@gmail.com>
2022-02-24 11:46:21 +0530

    Adding program to calculate elapsed time

---
+++ StopwatchProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace LogicalProgramming
+{
+    public class StopwatchProgram
+    {
+        public static void CalElapsedTime()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            // Doing something to spend the time
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed Time :  " + stopwatch.Elapsed);
+
+        }
+       
+
+        
+    }
+}
